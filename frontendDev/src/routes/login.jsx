e44b7ff02225ba07f8c72de40ba013d9899807d7
GoncaloCoutoDosSantos <a93279@alunos.uminho.pt>
2022-10-12 15:49:32 +0100

    fix login register page

--- frontendDev/src/routes/login.jsx
+++ frontendDev/src/routes/login.jsx
@@ -3,7 +3,8 @@ import {Link} from 'react-router-dom'
 export default function Login() {
   return (
     <p>
-    <h1>Login</h1>
+    <Link to='home'>Login</Link>
+    <h1 />
     <Link to='register'>Register</Link>
     </p>
   );
