b12bab3860607f17e6ad5cd8793f5ce36d5aa7ae
GoncaloCoutoDosSantos <a93279@alunos.uminho.pt>
2022-10-14 19:35:32 +0100

    rought autentication methoed

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,5 @@
 import { Outlet,Link,useLoaderData,Form,redirect,NavLink,useNavigation} from "react-router-dom";
 import { getContacts,createContact } from "../contacts";
-import Login from "./login";
 
 export async function loader({ request }) {
   const url = new URL(request.url);
@@ -14,21 +13,9 @@ export async function action() {
   return redirect(`/contacts/${contact.id}/edit`);
 }
 
-function getToken(){
-  const tokenString = sessionStorage.getItem('token');
-  console.log(tokenString);
-  const userToken = JSON.parse(tokenString);
-  console.log(userToken);
-  return userToken?.token;
-}
-
 export default function Root() {
   const { contacts } = useLoaderData();
   const navigation = useNavigation();
-  const token = getToken();
-
-  if(!token){console.log(token);return <Login />;}
-
   return (
     <>
       <div id="sidebar">
---
+++ src/routes/root_login.jsx
@@ -0,0 +1,9 @@
+import {Outlet} from 'react-router-dom'
+
+export default function Root_login() {
+	return(
+		<div>
+		<Outlet/>
+		</div>
+	);
+}
