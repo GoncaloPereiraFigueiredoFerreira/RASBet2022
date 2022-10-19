75128c7c4df5a37712e78c2609785868d413091e
GoncaloCoutoDosSantos <a93279@alunos.uminho.pt>
2022-10-19 15:22:41 +0100

    remove trash

--- frontendDev/src/index.jsx
+++ frontendDev/src/index.jsx
@@ -8,9 +8,6 @@ import {
 import "./index.css";
 import Root,{ loader as rootLoader,action as rootAction} from './routes/root.jsx';
 import ErrorPage from './error-page.jsx'
-import Contact,{ loader as contactLoader} from './routes/contact.jsx';
-import Edit,{action as editAction} from './routes/edit.jsx'
-import Destroy,{action as destroyAction} from './routes/destroy.jsx'
 import Index from './routes/index.jsx'
 
 import Root_login from './routes/root_login.jsx'
@@ -32,26 +29,9 @@ const router = createBrowserRouter([
   {
     path: "/home",
     element: <Root />,
-    loader: rootLoader,
-    action: rootAction,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Index /> },
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <Edit />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
-      }
     ],
   },
 ]);
