6e879502d7a0a2e246c6528f45e5880f2d0540e2
HugoFerreiraNogueira <hugoferreiranogueira@gmail.com>
2022-10-22 19:35:16 +0100

    DBcommunication update

--- backendDev/DBCommunication/DBcommunication.js
+++ backendDev/DBCommunication/DBcommunication.js
@@ -10,8 +10,8 @@ class DBCommunication {
        
         this.db = mysql.createConnection({
             host:"localhost",
-            user:"",
-            password:""
+            user:"root",
+            password:"ola123"
         });
 
         this.db.connect((err)=>{
@@ -38,42 +38,81 @@ class DBCommunication {
 
     transactionOnDb(transacao,callback){
     
-        if(transacao.Tipo=="AG" || transacao.Tipo=="DC"){
+        if(transacao.Tipo=="Aposta_Ganha" || transacao.Tipo=="Deposito_Conta"){
             console.log("a transacao é a AG ou DC")
             let sql='UPDATE Apostador SET Balance= Balance + ? WHERE Email= ? '
             this.db.query(sql,[transacao.Valor,transacao.ApostadorID],(err,result)=>{
-                if(err) throw err;
-                console.log(`Balanco da conta Email= ${transacao.ApostadorID} atualizado`)
-                sql= 'INSERT INTO Transacao SET ?'
-                this.db.query(sql,transacao,(err,result)=>{
+                try{
+
                     if(err) throw err;
-                    console.log("Transacao efetuada")
-                    return callback("Transacao efetuada")
-                })
+                    console.log(`Balanco da conta Email= ${transacao.ApostadorID} atualizado`)
+                    sql= 'INSERT INTO Transacao SET ?'
+                    this.db.query(sql,transacao,(err,result)=>{
+                        try{
+                            if(err) throw err;
+                            console.log("Transacao efetuada")
+                            return callback("Transacao efetuada")
+                        }
+                        catch(err){
+                            return callback({"error":err.code})
+                        }
+                        
+                    })
+                }
+                catch(err){
+                    return callback({"error":err.code})
+                }
+                
             })
         }
         else{
             console.log("a transacao é AP ou LC")
             let sql='SELECT Balance From Apostador Where Email= ? '
             this.db.query(sql,transacao.ApostadorID,(err,result)=>{
-                if(err) throw err;
-                console.log(result)
-                if(result[0].Balance<transacao.Valor){
-                    return callback("Not enough balance")
-                }
-                else{
-                    sql='UPDATE Apostador SET Balance= Balance - ? WHERE Email= ? '
-                    this.db.query(sql,[transacao.Valor,transacao.ApostadorID],(err,result)=>{
-                        if(err) throw err;
-                        console.log(`Balanco da conta Email= ${transacao.ApostadorID} atualizado`)
-                        sql= 'INSERT INTO Transacao SET ?'
-                        this.db.query(sql,transacao,(err,result)=>{
-                            if(err) throw err;
-                            console.log("Transacao efetuada")
-                            return callback("Transacao efetuada")
+                try{
+                    if(err) throw err;
+                    console.log(result)
+                    
+                    console.log(`balance-> ${result[0].Balance}`)
+
+                    console.log(`transacao.valor -> ${transacao.Valor}`)
+
+                    if(result[0].Balance<transacao.Valor){
+
+                        return callback({"error":"Not enough balance"})
+                    }
+                    else{
+                        sql='UPDATE Apostador SET Balance= Balance - ? WHERE Email= ? '
+                        this.db.query(sql,[transacao.Valor,transacao.ApostadorID],(err,result)=>{
+                            try{
+
+                                if(err) throw err;
+                                console.log(`Balanco da conta Email= ${transacao.ApostadorID} atualizado`)
+                                sql= 'INSERT INTO Transacao SET ?'
+                                this.db.query(sql,transacao,(err,result)=>{
+                                    try{
+                                        if(err) throw err;
+                                        console.log("Transacao efetuada")
+                                        return callback("Transacao efetuada")
+                                    }
+                                    catch(err){
+                                        return callback({"error":err.code})
+                                    }
+                                    
+                                })
+
+                            }
+                            catch(err){
+                                return callback({"error":err.code})
+                            }
+                            
                         })
-                    })
+                    }
                 }
+                catch(err){
+                    return callback({"error":err.code})
+                }
+                
             })
         }
     }
@@ -83,10 +122,10 @@ class DBCommunication {
         this.db.query(sql,apostador,(err,result)=>{
             try{
                 if(err) throw err;
-                return callback('Apostador adicionado')
+                return callback('fine')
             }
             catch(err){
-                return callback(err.code)
+                return callback({"error":err.code})
             }
         })
     }
@@ -110,47 +149,116 @@ class DBCommunication {
                                 return callback("Não existem essas credenciais na base de dados")
                             }
                             else{
-                                return callback(result)
+                                return callback({"FRole":"apostador"})
                             }
                         }
                         catch(err){
-                            return callback(err.code)
+                            return callback({"error":err.code})
                         }
                     }) 
                 }
                 else{
-                    return callback(result)
+                    let data = JSON.parse(JSON.stringify(result))
+                    return callback({'FRole':data[0].FRole})
                 }
             }
             catch(err){
-                return callback(err.code)
+                return callback({"error":err.code})
             }
         })
     }
+
+    // addcodeUsedOnDb(promocao_apostador,callback){
+    //     let sql='INSERT INTO Promocao_Apostador SET ?'
+    //     this.db.query(sql,promocao_apostador,(err,result)=>{
+    //         try{
+    //             if(err) throw err;
+    //             return callback("Codigo usado guardado")
+    //         }
+    //         catch(err){
+    //             return callback(err.code)
+    //         }
+            
+    //     })
+    // }
+    registerBetOnDb(aposta,evento,codigo,callback){
+        let db= this.db
+        //nao verifica se se pode usar codigo promocional
+
+        //regista a transação de aposta
+        this.transactionOnDb({"ApostadorID":aposta.ApostadorID,"Valor":aposta.Montante,"Tipo":"Aposta","DataTr":aposta.DateAp},function(result){
+
+            //regista a utilização da promoção por parte do apostador
+            if(result.error) return callback(result)
+
+            let sql= 'INSERT INTO Promocao_Apostador SET ?'
+            db.query(sql,{"Codigo":codigo,"ApostadorID":aposta.ApostadorID},(err,result)=>{
+                try{
+                    if(err) throw err
+
+                    //procura o valor do codigo aplicado para depois somar ao montante da aposta
+
+                    sql='SELECT Valor FROM Promocao WHERE Codigo = ?'
+                    db.query(sql,codigo,(err,result)=>{
+                        try{
+                            if(err) throw err
+                            let data = JSON.parse(JSON.stringify(result))
+                            aposta.Montante+= data[0].Valor
+
+                            //regista a aposta
+
+                            sql= 'INSERT INTO Aposta SET ?'
+                            db.query(sql,aposta,(err,result)=>{
+                                try{
+                                    if(err) throw err;
+
+                                    //devolve o último id inserido por auto_increment
+
+                                    sql= 'SELECT LAST_INSERT_ID() as LastID'
+                                    db.query(sql,(err,result)=>{
+                                        try{
+                                            if(err) throw err;
+                                            var data=JSON.parse(JSON.stringify(result))
+                                            //console.log(data[0])
+                                            //console.log(data[0].LastID)
+
+                                            //regista na table aposta_evento 
+
+                                            sql= 'INSERT INTO Aposta_Evento SET `ApostaID` = ? , ?'
+                                            db.query(sql,[data[0].LastID,evento],(err,result)=>{
+                                                try{
+                                                    if(err) throw err;
+                                                    return callback('Aposta adicionada')
+
+                                                }
+                                                catch(err){
+                                                    console.log(err)
+                                                    return callback(err.code)
+                                                }
+                                            }) 
+                                        }
+                                        catch(err){
+                                            console.log(err)
+                                            return callback(err.code)
+                                        }
+                                    })
+                                }
+                                catch(err){
+                                    return callback(err.code)
+                                }
+                            })
+                        }
+                        catch(err){
+                        }
+                    })
+                }
+                catch(err){
+                }
+            })
+        })
+    }
+
     
-    registerBetOnDb(aposta,aposta_evento,callback){
-        let sql= 'INSERT INTO Aposta SET ?'
-        this.db.query(sql,aposta,(err,result)=>{
-            try{
-                if(err) throw err;
-                sql= 'INSERT INTO Aposta_Evento SET ?'
-                this.db.query(sql,aposta_evento,(err,result)=>{
-                    try{
-                        if(err) throw err;
-                        return callback('Aposta_Evento adicionada')
-                    }
-                    catch{
-                        return callback(err.code)
-                    }
-                })
-            }
-            catch(err){
-                return callback(err.code)
-            }
-            
-            
-        })
-    }
     
     editProfileOnDb(list,email,callback){
         let sql =`UPDATE Apostador SET ${list} Where Email=?`
@@ -198,7 +306,6 @@ class DBCommunication {
         this.db.query(sql,evento,(err,result)=>{
             try{
                 if(err) throw err;
-                console.log("try")
                 return callback("Evento adicionado")
             }
             catch(err){
@@ -223,19 +330,7 @@ class DBCommunication {
         })
     }
     
-    addcodeUsedOnDb(promocao_apostador,callback){
-        let sql='INSERT INTO Promocao_Apostador SET ?'
-        this.db.query(sql,promocao_apostador,(err,result)=>{
-            try{
-                if(err) throw err;
-                return callback("Codigo usado guardado")
-            }
-            catch(err){
-                return callback(err.code)
-            }
-            
-        })
-    }
+    
     
     usedCodOnDb(email,codigo,callback){
         let sql='SELECT * FROM Promocao_Apostador WHERE Codigo=? AND ApostadorID = ?'
--- backendDev/DBCommunication/bd.sql
+++ backendDev/DBCommunication/bd.sql
@@ -19,6 +19,7 @@ CREATE TABLE IF NOT EXISTS Apostador (
     DataNascimento DATE NOT NULL,
     Balance decimal(15,2) NOT NULL,
     PlvPasse VARCHAR(75) NOT NULL,
+    CC VARCHAR(10) NOT NULL,
     PRIMARY KEY(Email)   
 );
 
@@ -42,14 +43,13 @@ CREATE TABLE IF NOT EXISTS Transacao(
     ID INT NOT NULL AUTO_INCREMENT,
     ApostadorID VARCHAR(75) NOT NULL,
     Valor decimal(15,2) NOT NULL,
-    Tipo ENUM("AG", "AP","LC","DC") NOT NULL,
+    Tipo ENUM("Aposta_Ganha", "Aposta","Levantamento_Conta","Deposito_Conta") NOT NULL,
     DataTr DATETIME NOT NULL,
     PRIMARY KEY(ID),
     FOREIGN KEY(ApostadorID)
         REFERENCES Apostador(Email)
 
 );
-
 
 CREATE TABLE IF NOT EXISTS Aposta (
     ID INT NOT NULL AUTO_INCREMENT,
@@ -65,26 +65,25 @@ CREATE TABLE IF NOT EXISTS Aposta (
 );
 
 CREATE TABLE IF NOT EXISTS Evento(
-    ID INT NOT NULL,
+    ID VARCHAR(25) NOT NULL,
+    Desporto ENUM("FUT","FUTPT","BSK","F1") NOT NULL,
     Resultado INT NOT NULL,
     Descricao VARCHAR(255),
     Estado ENUM("Nao_iniciado","Finalizado") NOT NULL,
-    Desporto ENUM("FB","F1","TN","BB") NOT NULL,
+    Liga VARCHAR(50),
     DataEvent DATETIME NOT NULL,
     PRIMARY KEY(ID,Desporto)
 );
 
 CREATE TABLE IF NOT EXISTS Aposta_Evento(
 	ApostaID INT NOT NULL,
-    EventoID INT NOT NULL,
-    Desporto  ENUM("FB","F1","TN","BB") NOT NULL,
+    EventoID VARCHAR(25) NOT NULL,
+    Desporto ENUM("FUT","FUTPT","BSK","F1") NOT NULL,
     PRIMARY KEY(ApostaID,EventoID),
     FOREIGN KEY(ApostaID)
 		REFERENCES Aposta(ID),
-	FOREIGN KEY(EventoID)
-		REFERENCES Evento(ID),
-    FOREIGN KEY(EventoID)
-        REFERENCES Evento(Desporto)
+    FOREIGN KEY(EventoID,Desporto)
+		REFERENCES Evento(ID,Desporto)
 );
 
 
