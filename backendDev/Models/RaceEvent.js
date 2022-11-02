f172233c749b42571ff3d4e628f87bae0c8bf000
GoncaloPereiraFigueiredoFerreira <a93218@alunos.uminho.pt>
2022-11-02 22:00:58 +0000

    Added circuit photo

--- backendDev/Models/RaceEvent.js
+++ backendDev/Models/RaceEvent.js
@@ -5,7 +5,7 @@ class RaceEvent extends SportEvent{
     constructor(sport,league,id,description,result,state, datetime, pilots,pilotsPhotos, circuit,circuitPhoto){
         
         super(sport,league,id,description,result,state,datetime);
-        this.PlayerOdds = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+        this.PlayerOdds = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
         this.Pilots = pilots;
         this.PilotsPht = pilotsPhotos;
         this.Circuit = circuit;
@@ -26,6 +26,7 @@ class RaceEvent extends SportEvent{
     }
 
     toJson(){
+        // Puts the circuit photo in the last of the logos
         let photos = [];
         this.PilotsPht.map(x=>{photos.push(x)});
         photos.push(this.CircuitPht)
@@ -33,7 +34,7 @@ class RaceEvent extends SportEvent{
             "Tipo": "RaceEvent",
             "EventoId" : this.Id,
             "Liga" : this.Circuit,
-            "Participantes" : this.Pilots, //Foto no fim
+            "Participantes" : this.Pilots, 
             "Odds" : this.PlayerOdds,
             "Logos": photos,
             "Data" : this.DateTime,
