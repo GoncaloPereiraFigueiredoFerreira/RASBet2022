0c6bae3a6f0396d3292fe8cad8b0e5e612b93c9c
DivineWingedDemon <kikodabeira@gmail.com>
2023-01-17 17:00:09 +0000

    more one astetic improvement

--- frontendDev/src/templates/empate.jsx
+++ frontendDev/src/templates/empate.jsx
@@ -31,9 +31,21 @@ export default function Empate({evento,addAposta,escolhas,follow,clickFollow}){
 					  <p className="betinfo">{parseDate(evento.Data)}</p>
 		            </div>
 		            <div className="drawmatchodds">
-				            <button className='odd-button' style={{"backgroundColor":(escolhas.includes(0))?"orange":"beige","color":(escolhas.includes(0))?"white":"black"}} onClick={()=>(addAposta(parseBet(evento,0)))}>home {evento.Odds[0].toFixed(2)}</button>
-			              	<button className='odd-button' style={{"backgroundColor":(escolhas.includes(2))?"orange":"beige","color":(escolhas.includes(2))?"white":"black"}} onClick={()=>(addAposta(parseBet(evento,2)))}>draw {evento.Odds[2].toFixed(2)}</button>
-							<button className='odd-button' style={{"backgroundColor":(escolhas.includes(1))?"orange":"beige","color":(escolhas.includes(1))?"white":"black"}} onClick={()=>(addAposta(parseBet(evento,1)))}>away {evento.Odds[1].toFixed(2)}</button>
+				            <button className='odd-button' 
+								style={(!evento.SuperOdds)?
+									{"backgroundColor":(escolhas.includes(0))?"orange":"beige","color":(escolhas.includes(0))?"white":"black"}:
+									{"backgroundColor":(escolhas.includes(0))?"purple":"plum","color":(escolhas.includes(0))?"white":"black"}} 
+									onClick={()=>(addAposta(parseBet(evento,0)))}>home {evento.Odds[0].toFixed(2)}</button>
+			              	<button className='odd-button' 
+								style={(!evento.SuperOdds)?
+									{"backgroundColor":(escolhas.includes(2))?"orange":"beige","color":(escolhas.includes(2))?"white":"black"}:
+									{"backgroundColor":(escolhas.includes(2))?"purple":"plum","color":(escolhas.includes(2))?"white":"black"}} 
+									onClick={()=>(addAposta(parseBet(evento,2)))}>draw {evento.Odds[2].toFixed(2)}</button>
+							<button className='odd-button' 
+								style={(!evento.SuperOdds)?
+									{"backgroundColor":(escolhas.includes(1))?"orange":"beige","color":(escolhas.includes(1))?"white":"black"}:
+									{"backgroundColor":(escolhas.includes(1))?"purple":"plum","color":(escolhas.includes(1))?"white":"black"}} 
+									onClick={()=>(addAposta(parseBet(evento,1)))}>away {evento.Odds[1].toFixed(2)}</button>
 		            	<div style={{"display":"flex","flexDirection":"column","justifyContent":"space-between"}}>
 			            	<img src={(follow)?'/follow_on.png':'/follow_off.png'} style={{"height":"30px","cursor":"pointer"}} onClick={()=>(clickFollow(follow,evento.EventoId))}/>
 
